3435be0ff90680a26b80fe27857a193124299d8f
kavya <kavyadsc@gmail.com>
2025-06-11 19:16:01 +0530

    EDA and problem statements

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -16,7 +16,7 @@ if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.error("Divided By Zero Error")
+        logging.info("Divided By Zero Error")
         raise CustomException(e,sys)
         
     
--- src/logger.py
+++ src/logger.py
@@ -17,3 +17,6 @@ logging.basicConfig(
 )
 if __name__=="__main__":
     logging.info("logging has started")
+
+
+    
